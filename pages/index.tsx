69701b2f24621cab6b59f912d2774d1a214873ce
Amorem <dev@amorem.com>
2021-05-21 10:58:34 +0200

    remove Prisma integration

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,33 +3,9 @@ import Head from 'next/head';
 import AddContactForm from './../components/AddContactForm';
 import ContactCard from './../components/ContactCard';
 
-import { PrismaClient, Contact, Prisma } from '@prisma/client';
-
-const prisma = new PrismaClient();
-
-export async function getServerSideProps() {
-  const contacts: Contact[] = await prisma.contact.findMany();
-  return {
-    props: {
-      initialContacts: contacts
-    }
-  };
-}
-
-async function saveContact(contact: Prisma.ContactCreateInput) {
-  const response = await fetch('/api/contacts', {
-    method: 'POST',
-    body: JSON.stringify(contact)
-  });
-
-  if (!response.ok) {
-    throw new Error(response.statusText);
-  }
-  return await response.json();
-}
 
 export default function Index({ initialContacts }) {
-  const [contacts, setContacts] = useState<Contact[]>(initialContacts);
+  const [contacts, setContacts] = useState([]);
   return (
     <>
       <Head>
@@ -48,7 +24,7 @@ export default function Index({ initialContacts }) {
           <AddContactForm
             onSubmit={async (data, e) => {
               try {
-                await saveContact(data);
+                
                 setContacts([...contacts, data]);
                 e.target.reset();
               } catch (err) {
@@ -61,7 +37,7 @@ export default function Index({ initialContacts }) {
           <div className="mb-3">
             <h2 className="text-3xl text-gray-700">Contacts</h2>
           </div>
-          {contacts.map((c, i: number) => (
+          {contacts?.map((c, i: number) => (
             <div className="mb-3" key={i}>
               <ContactCard contact={c} />
             </div>
